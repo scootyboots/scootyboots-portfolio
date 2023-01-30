c4a7bca814e7b4981e4fff3e730de627398ffea7
scoffrin <scoffrin@indeed.com>
2023-01-30 21:08:12 +0900

    remove boilerplate landing page

--- src/pages/art.tsx
+++ src/pages/art.tsx
@@ -1,7 +1,26 @@
+import worksData from "../works-data.json";
+import Image from "next/image";
+
 export default function Art() {
   return (
     <div>
       <h1>this is where the art portfosadsdsadsal a sdsd goes</h1>
+      <h2>number of works {worksData.length}</h2>
+      {worksData.map((workData, i) => (
+        <>
+          <div key={workData.fileName}>
+            <Image
+              src={"/images/" + workData.fileName}
+              alt="painting"
+              width={1200}
+              height={1200}
+            />
+            {workData.fileName}
+            {workData.title}
+            {workData.year}
+          </div>
+        </>
+      ))}
     </div>
   );
 }
