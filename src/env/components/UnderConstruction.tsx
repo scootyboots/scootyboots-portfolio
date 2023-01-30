6a4d5bbba0e3f6eca96c1b0b68551a95bc7033f5
scoffrin <scoffrin@indeed.com>
2023-01-30 23:07:39 +0900

    styled constuction page

--- src/env/components/UnderConstruction.tsx
+++ src/env/components/UnderConstruction.tsx
@@ -1,3 +1,42 @@
 export default function UnderConstruction() {
-  return <>this site is under construction, come back some other time</>;
+  const centerMessage = (
+    <div className="absolute flex h-screen w-screen items-center justify-center">
+      <div className="text-4xl font-bold text-gray-100">
+        this site is under construction, come back some other time
+      </div>
+    </div>
+  );
+  return (
+    <div className="flex h-screen w-screen flex-wrap bg-gray-800">
+      <div className="relative flex h-[85vh] w-[14.28vw] bg-gray-300"></div>
+      <div className="relative flex h-[85vh] w-[14.28vw] items-center justify-center bg-yellow-400">
+        <div className="text-4xl font-bold text-gray-100">under</div>
+      </div>
+      <div className="relative flex h-[85vh] w-[14.28vw] items-center justify-center bg-blue-400">
+        <div className="text-4xl font-bold text-gray-100">cons</div>
+      </div>
+      <div className="relative flex h-[85vh] w-[14.28vw] items-center justify-center bg-green-400">
+        <div className="text-4xl font-bold text-gray-100">truc</div>
+      </div>
+      <div className="relative flex h-[85vh] w-[14.28vw] items-center justify-center bg-pink-500">
+        <div className="text-4xl font-bold text-gray-100">tion</div>
+      </div>
+      <div className="relative flex h-[85vh] w-[14.28vw] items-center justify-center bg-red-700">
+        <div className="text-4xl font-bold text-gray-100">come</div>
+      </div>
+      <div className="relative flex h-[85vh] w-[14.28vw] items-center justify-center bg-blue-700">
+        <div className="text-4xl font-bold text-gray-100">back</div>
+      </div>
+      <div className="flex w-full">
+        <div className="h-[15vh] w-[17.85vw] bg-blue-900"></div>
+        <div className="h-[15vh] w-[17.85vw] bg-white"></div>
+        <div className="h-[15vh] w-[17.85vw] bg-purple-900"></div>
+        <div className="h-[15vh] w-[17.85vw] bg-gray-700"></div>
+        <div className="h-[15vh] w-[4.76vw] bg-gray-900"></div>
+        <div className="h-[15vh] w-[4.76vw] bg-gray-800"></div>
+        <div className="h-[15vh] w-[4.76vw] bg-gray-700"></div>
+        <div className="h-[15vh] w-[14.28vw] bg-gray-900"></div>
+      </div>
+    </div>
+  );
 }
